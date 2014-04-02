1c6f6fee96d837f59caa35da20d48a1feef330c1
Jeremy Archer <open-source@fatlotus.com>
2014-04-01 22:00:41 -0500

    Include freetype in Vagrant image.

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 
 apt-get update -y
 apt-get install -y python-numpy python-scipy build-essential python2.7 curl git\
-    libfftw3-dev python-matplotlib python-pip libblas-dev python-virtualenv
+    libfftw3-dev python-matplotlib python-pip libblas-dev python-virtualenv\
+    libfreetype6-dev
