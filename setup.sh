ddd01b2b6d39066f10bb4905d9b3c4fe57a78395
Jeremy Archer <open-source@fatlotus.com>
2014-04-01 21:16:06 -0500

    Add virtualenv to dependencies.

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 apt-get update -y
 apt-get install -y python-numpy python-scipy build-essential python2.7 curl git\
-    libfftw3-dev python-matplotlib python-pip libblas-dev
+    libfftw3-dev python-matplotlib python-pip libblas-dev python-virtualenv
